475a03ffa7136d0f30f66c158e00b2631ed560ce
Dana Li <halfwayhackathon@gmail.com>
2016-03-09 20:37:03 -0800

    first commit

---
+++ JS/logic.js
@@ -0,0 +1,84 @@
+var vegetarians = [
+	{
+		//index: 1,
+		name: 'Veggie Meatballs',
+		link: 'https://www.youtube.com/embed/kJrQvTzQpog',
+		recipe: ' ',
+	},
+	{
+		//index: 2,
+		name: 'Veggie Burgers',
+		link: 'https://www.youtube.com/embed/2b_LTaBbZw0',
+		recipe: ' ',
+	},
+	{
+		//index: 3,
+		name: 'Veggie Spaghetti',
+		link: 'https://www.youtube.com/embed/M6ol_t88T9Q',
+		recipe: ' ',
+	}
+]
+
+
+var meats = [
+	{
+		//index: 1,
+		name: 'Chicken Fettuccine',
+		link: 'https://www.youtube.com/embed/5p7avk-9IwE',
+		recipe: ' ',
+	},
+	{
+		//index: 2,
+		name: 'Beef Stroganoff',
+		link: 'https://www.youtube.com/embed/2J482y8WVdo',
+		recipe: ' ',
+	},
+	{
+		//index: 3,
+		name: 'Fish and Chips',
+		link: 'https://www.youtube.com/embed/I8ckIq2-j0k',
+		recipe: ' ',
+	}
+]
+
+
+
+
+var lottery = Math.floor(Math.random()*3 + 1);
+
+//$('.recipeName').append(meats[lottery-1].name);
+//$('.recipeLink').attr('src', meats[lottery-1].link);
+
+function showMeat(){
+	$('.main').append("<div class = 'row text-center'><div class = 'col-md-12'><h3 class = 'recipeName'></h3><iframe class = 'recipeLink'></iframe></div></div>");
+	$('.recipeName').text('Recipe: ' + meats[lottery-1].name);
+	$('.recipeLink').attr('src', meats[lottery-1].link);
+}
+
+function showVeggie(){
+	$('.main').append("<div class = 'row text-center'><div class = 'col-md-12'><h3 class = 'recipeName'></h3><iframe class = 'recipeLink'></iframe></div></div>");
+	$('.recipeName').text('Recipe: ' + vegetarians[lottery-1].name);
+	$('.recipeLink').attr('src', vegetarians[lottery-1].link);
+}
+
+function showMain(){
+
+}
+//$('#meat').on('click', showMeat());
+//$('#veggie').on('click', showVeggie());
+
+$('button').on('click', function(){
+	//event.preventDefault();
+	var id = this.id;
+	if(id === 'veggie'){
+		//console.log('veggie');
+		$('.main').empty();
+		showVeggie();
+	}else if(id === 'meat'){
+		//console.log('meat');
+		$('.main').empty();
+		showMeat();
+	}
+}); 
+
+
