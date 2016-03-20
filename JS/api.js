b54120ec5edd617d8f8ac3db8b36a90d9958415b
Dana Li <halfwayhackathon@gmail.com>
2016-03-19 18:27:41 -0700

    new search query

--- JS/api.js
+++ JS/api.js
@@ -1,3 +1,62 @@
+//Helper Function
+function randomIndex(){
+	return Math.floor(Math.random()*20);
+}
+
+//Click Handler for Veggie button
+$('#veggie').on('click', function(e){
+	e.preventDefault();
+	$('.main').empty();
+	var request = gapi.client.youtube.search.list({
+		part : ' snippet',
+		type: 'video',
+		q: encodeURIComponent('vegetarian recipe').replace(/%20/g, '+'),
+		maxResults: 20,
+		order: 'viewCount',
+		publishedAfter: '2015-01-01T00:00:00Z'
+	});
+	request.execute(function(response){
+		//console.log(response);
+		var results = response.result;
+		var index = randomIndex();
+		console.log(index);
+		$('.main').append("<div class = 'row text-center'><div class = 'col-md-12'><h3 class = 'recipeName'></h3><iframe class = 'recipeLink'></iframe></div></div>");
+		$('.recipeName').text('Recipe: ' + results.items[index].snippet.title);
+		$('.recipeLink').attr('src', 'https://www.youtube.com/embed/'+ results.items[index].id.videoId);	
+	})
+})
+
+//Click Handler for Meat Button
+$('#meat').on('click', function(e){
+	e.preventDefault();
+	$('.main').empty();
+	var request = gapi.client.youtube.search.list({
+		part : ' snippet',
+		type: 'video',
+		q: encodeURIComponent('meat recipes cooking').replace(/%20/g, '+'),
+		maxResults: 20,
+		order: 'viewCount',
+		publishedAfter: '2015-01-01T00:00:00Z'
+	});
+	request.execute(function(response){
+		//console.log(response);
+		var results = response.result;
+		var index = randomIndex();
+		$('.main').append("<div class = 'row text-center'><div class = 'col-md-12'><h3 class = 'recipeName'></h3><iframe class = 'recipeLink'></iframe></div></div>");
+		$('.recipeName').text('Recipe: ' + results.items[index].snippet.title);
+		$('.recipeLink').attr('src', 'https://www.youtube.com/embed/'+ results.items[index].id.videoId);	
+	})
+})
+
+
+function init(){
+	gapi.client.setApiKey('AIzaSyDgCqI74Acb4UY7GPhZ3-Sz8jal_F2OwKE');
+	gapi.client.load('youtube', 'v3', function(){
+		//api is ready
+	})
+}
+
+
 // $('form').on('submit', function(e){
 // 	e.preventDefault();
 // 	var request = gapi.client.youtube.search.list({
@@ -16,58 +75,3 @@
 // 		})
 // 	})
 // })
-
-function randomIndex(){
-	return Math.floor(Math.random()*10);
-}
-
-$('#veggie').on('click', function(e){
-	e.preventDefault();
-	$('.main').empty();
-	var request = gapi.client.youtube.search.list({
-		part : ' snippet',
-		type: 'video',
-		q: encodeURIComponent('vegetarian recipe').replace(/%20/g, '+'),
-		maxResults: 10,
-		order: 'viewCount',
-		publishedAfter: '2015-01-01T00:00:00Z'
-	});
-	request.execute(function(response){
-		//console.log(response);
-		var results = response.result;
-		var index = randomIndex();
-		console.log(index);
-		$('.main').append("<div class = 'row text-center'><div class = 'col-md-12'><h3 class = 'recipeName'></h3><iframe class = 'recipeLink'></iframe></div></div>");
-		$('.recipeName').text('Recipe: ' + results.items[index].snippet.title);
-		$('.recipeLink').attr('src', 'https://www.youtube.com/embed/'+ results.items[index].id.videoId);	
-	})
-})
-
-$('#meat').on('click', function(e){
-	e.preventDefault();
-	$('.main').empty();
-	var request = gapi.client.youtube.search.list({
-		part : ' snippet',
-		type: 'video',
-		q: encodeURIComponent('dinner recipe').replace(/%20/g, '+'),
-		maxResults: 10,
-		order: 'viewCount',
-		publishedAfter: '2015-01-01T00:00:00Z'
-	});
-	request.execute(function(response){
-		//console.log(response);
-		var results = response.result;
-		var index = randomIndex();
-		$('.main').append("<div class = 'row text-center'><div class = 'col-md-12'><h3 class = 'recipeName'></h3><iframe class = 'recipeLink'></iframe></div></div>");
-		$('.recipeName').text('Recipe: ' + results.items[index].snippet.title);
-		$('.recipeLink').attr('src', 'https://www.youtube.com/embed/'+ results.items[index].id.videoId);	
-	})
-})
-
-
-function init(){
-	gapi.client.setApiKey('AIzaSyDgCqI74Acb4UY7GPhZ3-Sz8jal_F2OwKE');
-	gapi.client.load('youtube', 'v3', function(){
-		//yt api is ready
-	})
-}
