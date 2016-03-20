3b8b4c326f521c7c1c2178b1a72bc4e598386af3
Dana Li <halfwayhackathon@gmail.com>
2016-03-19 17:36:04 -0700

    add meat button

--- JS/api.js
+++ JS/api.js
@@ -16,11 +16,10 @@
 // 		})
 // 	})
 // })
+
 function randomIndex(){
 	return Math.floor(Math.random()*10);
 }
-
-
 
 $('#veggie').on('click', function(e){
 	e.preventDefault();
@@ -34,14 +33,33 @@ $('#veggie').on('click', function(e){
 		publishedAfter: '2015-01-01T00:00:00Z'
 	});
 	request.execute(function(response){
+		//console.log(response);
 		var results = response.result;
 		var index = randomIndex();
 		$('.main').append("<div class = 'row text-center'><div class = 'col-md-12'><h3 class = 'recipeName'></h3><iframe class = 'recipeLink'></iframe></div></div>");
+		$('.recipeName').text('Recipe: ' + results.items[index].snippet.title);
+		$('.recipeLink').attr('src', 'https://www.youtube.com/embed/'+ results.items[index].id.videoId);	
+	})
+})
+
+$('#meat').on('click', function(e){
+	e.preventDefault();
+	$('.main').empty();
+	var request = gapi.client.youtube.search.list({
+		part : ' snippet',
+		type: 'video',
+		q: encodeURIComponent('meat dinner recipe').replace(/%20/g, '+'),
+		maxResults: 1,
+		order: 'viewCount',
+		publishedAfter: '2015-01-01T00:00:00Z'
+	});
+	request.execute(function(response){
+		//console.log(response);
+		var results = response.result;
+		//var index = randomIndex();
+		$('.main').append("<div class = 'row text-center'><div class = 'col-md-12'><h3 class = 'recipeName'></h3><iframe class = 'recipeLink'></iframe></div></div>");
 		$('.recipeName').text('Recipe: ' + results.items[0].snippet.title);
-		$('.recipeLink').attr('src', 'https://www.youtube.com/embed/'+ results.items[0].id.videoId);
-		// $.each(results.items, function(index,item){	
-		// 	$('#results').append(item.id.videoId+' '+item.snippet.title+'<br>');
-		// })
+		$('.recipeLink').attr('src', 'https://www.youtube.com/embed/'+ results.items[0].id.videoId);	
 	})
 })
 
