401140d9a33e9fcabc2d5faf2acd41051223040d
Dana Li <halfwayhackathon@gmail.com>
2016-03-19 19:06:10 -0700

    try back button

--- JS/api.js
+++ JS/api.js
@@ -1,6 +1,17 @@
-//Helper Function
+//Helper Function: generate random index 
 function randomIndex(){
 	return Math.floor(Math.random()*20);
+}
+
+//render 'back' button on the DOM:
+function backButton(){
+	var $back = $('<button>').attr({
+		id: 'back',
+		type: 'button',
+		class: "btn btn-default main"
+	});
+	$back.text('Go Back');
+	$('.container').append($back);
 }
 
 //Click Handler for Salad button
@@ -19,11 +30,12 @@ $('#salad').on('click', function(e){
 		//console.log(response);
 		var results = response.result;
 		var index = randomIndex();
-		console.log(index);
+		
 		$('.main').append("<div class = 'row text-center'><div class = 'col-md-12'><h3 class = 'recipeName'></h3><iframe class = 'recipeLink'></iframe></div></div>");
 		$('.recipeName').text('Recipe: ' + results.items[index].snippet.title);
 		$('.recipeLink').attr('src', 'https://www.youtube.com/embed/'+ results.items[index].id.videoId);	
 	})
+	backButton()
 })
 
 //Click Handler for soup Button
