910c6660f95fd9b3b621cec8a1354729e5cfe7a3
Dana Li <halfwayhackathon@gmail.com>
2016-03-25 16:03:00 -0700

    fix syntax error

--- JS/api.js
+++ JS/api.js
@@ -25,7 +25,7 @@ $('#test').on('click', function(e){
 	
 		var element = document.getElementById('main');
 		element.innerHTML = "<div class = 'row text-center'><div class = 'col-md-12'><h3 class = 'recipeName'></h3><iframe class = 'recipeLink'></iframe></div></div>";
-		element.getElementsByTagName('H3').innerHTML = item[index].snippet.title);
+		element.getElementsByTagName('H3').innerHTML = item[index].snippet.title;
 		element.getElementsByTagName('iframe').src =  'https://www.youtube.com/embed/'+ results.items[index].id.videoId;
 		
 	})
